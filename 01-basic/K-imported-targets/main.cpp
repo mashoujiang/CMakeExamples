6c6874ac0a01590e447be6c2572509e88bae0b38
Murphy <1067295896@qq.com>
2020-07-19 20:39:56 +0800

    Implement 01-basic/K-imported-targets
    Signed-off-by: Murphy <1067295896@qq.com>

---
+++ 01-basic/K-imported-targets/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+#include <boost/filesystem.hpp>
+
+int main(int argc, char* argv[]){
+    std::cout << "hello third party include!" << std::endl;
+    boost::shared_ptr<int> isp(new int(4));
+    boost::filesystem::path path = "/usr/share/cmake/modules";
+    if(path.is_relative()){
+        std::cout << "path is relative" << std::endl;
+    }
+    else
+    {
+        std::cout << "path is not relative" << std::endl;
+    }
+
+    return 0;
+}
+
