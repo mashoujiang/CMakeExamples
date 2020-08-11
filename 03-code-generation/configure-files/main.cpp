f3d8d6bf52bd35712af9ee11460acd10c4fe92f7
Murphy <1067295896@qq.com>
2020-08-11 19:55:22 +0800

    Implement 03-code-generation
    Signed-off-by: Murphy <1067295896@qq.com>

---
+++ 03-code-generation/configure-files/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "ver.h"
+#include "path.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "hello version " << ver << std::endl;
+    std::cout << "path is " << path << std::endl;
+
+    return 0;
+}
